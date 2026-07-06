e0687e13610fd96887e51c5cbd3596824fd04619
softcookiepp <softcookiepp@mailfence.com>
2026-07-06 09:11:02 -0700

    up

--- src/kernels-vk/level3/xgemm_direct_strided_batched_nt.glsl
+++ src/kernels-vk/level3/xgemm_direct_strided_batched_nt.glsl
@@ -12,7 +12,7 @@
 // Enables loading of this file using the C++ pre-processor's #include (C++11 standard raw string
 // literal). Comment-out this line for syntax-highlighting when developing.
 //R"(
-#include "xgemm_direct_part3_strided_batched.glsl"
+#include "xgemm_direct_part3.glsl"
 // =================================================================================================
 
 // =================================================================================================

7bc35c72378ba73b9643c1013d520cea2acf4c67
softcookiepp <softcookiepp@mailfence.com>
2026-09-25 15:38:12 -0700

    convgemm got broken somehow, need to fix later

--- src/routines/levelx/xconvgemm.cpp
+++ src/routines/levelx/xconvgemm.cpp
@@ -104,7 +104,8 @@ void Xconvgemm<T>::DoConvgemm(const KernelMode kernel_mode, const size_t channel
 	// Possible approach: im2col + GEMM
 	//			result = GEMM(im2col(image), kernel)
 	auto col_buffer = Buffer<T>(queue_(), 0);	// nullptr, will be optionally created later
-	if (method_ == ConvGemmMethod::kWithIm2Col) {
+	if (method_ == ConvGemmMethod::kWithIm2Col)
+	{
 		// Temporary col matrix
 		const auto col_size = (method_ == ConvGemmMethod::kWithIm2Col) ? patch_size * num_patches * batch_count : 1;
 		col_buffer = Buffer<T>(queue_(), col_size);

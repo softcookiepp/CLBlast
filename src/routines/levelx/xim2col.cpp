66ba09da6ede7a213e2bca28ff09f78e8bc39931
softcookiepp <softcookiepp@protonmail.com>
2026-09-26 11:14:01 -0700

    im2col/col2im now use direct kernel invocations

--- src/routines/levelx/xim2col.cpp
+++ src/routines/levelx/xim2col.cpp
@@ -1,7 +1,7 @@
 
 // =================================================================================================
 // This file is part of the CLBlast project. Author(s):
-//   Cedric Nugteren <www.cedricnugteren.nl>
+//	 Cedric Nugteren <www.cedricnugteren.nl>
 //
 // This file implements the Xim2col class (see the header for information about the class).
 //
@@ -25,8 +25,8 @@ namespace clblast {
 // Constructor: forwards to base class constructor
 template <typename T>
 Xim2col<T>::Xim2col(Queue& queue, EventPointer event, const std::string& name)
-    : Routine(queue, event, name, {"Copy"}, PrecisionValue<T>(), {},
-              {
+		: Routine(queue, event, name, {"Copy"}, PrecisionValue<T>(), {},
+							{
 #if VULKAN_API
 	#include "../../kernels-vk-inline/levelx/im2col_kernel_flip.glsl.inl"
 	,
@@ -34,12 +34,12 @@ Xim2col<T>::Xim2col(Queue& queue, EventPointer event, const std::string& name)
 #else
 	#include "../../kernels/levelx/im2col.opencl"
 #endif
-              }
+							}
 #if VULKAN_API
 ,
 	 {"Xim2colKernelFlip", "Xim2colKernelNormal"}
 #endif
-		  ) {
+			) {
 }
 
 // =================================================================================================
@@ -47,55 +47,55 @@ Xim2col<T>::Xim2col(Queue& queue, EventPointer event, const std::string& name)
 // The main routine
 template <typename T>
 void Xim2col<T>::DoIm2col(const KernelMode kernel_mode, const size_t channels, const size_t height, const size_t width,
-                          const size_t kernel_h, const size_t kernel_w, const size_t pad_h, const size_t pad_w,
-                          const size_t stride_h, const size_t stride_w, const size_t dilation_h,
-                          const size_t dilation_w, const Buffer<T>& im_buffer, const size_t im_offset,
-                          const Buffer<T>& col_buffer, const size_t col_offset)
+													const size_t kernel_h, const size_t kernel_w, const size_t pad_h, const size_t pad_w,
+													const size_t stride_h, const size_t stride_w, const size_t dilation_h,
+													const size_t dilation_w, const Buffer<T>& im_buffer, const size_t im_offset,
+													const Buffer<T>& col_buffer, const size_t col_offset)
 {
-  // Flip the output along kernel_h and kernel_w, or not.
-  const auto kernel_name = (kernel_mode == KernelMode::kConvolution) ? "Xim2colKernelFlip" : "Xim2colKernelNormal";
+	// Flip the output along kernel_h and kernel_w, or not.
+	const auto kernel_name = (kernel_mode == KernelMode::kConvolution) ? "Xim2colKernelFlip" : "Xim2colKernelNormal";
 
-  // Makes sure all dimensions are larger than zero
-  if ((channels == 0) || (height == 0) || (width == 0)) {
-    throw BLASError(StatusCode::kInvalidDimension);
-  }
+	// Makes sure all dimensions are larger than zero
+	if ((channels == 0) || (height == 0) || (width == 0)) {
+		throw BLASError(StatusCode::kInvalidDimension);
+	}
 
-  // Sets the height and width of the 'col' result
-  const auto size_h = height + 2 * pad_h;
-  const auto padding_h = dilation_h * (kernel_h - 1) + 1;
-  const auto col_h = (size_h >= padding_h) ? (size_h - padding_h) / stride_h + 1 : 1;
-  const auto size_w = width + 2 * pad_w;
-  const auto padding_w = dilation_w * (kernel_w - 1) + 1;
-  const auto col_w = (size_w >= padding_w) ? (size_w - padding_w) / stride_w + 1 : 1;
+	// Sets the height and width of the 'col' result
+	const auto size_h = height + 2 * pad_h;
+	const auto padding_h = dilation_h * (kernel_h - 1) + 1;
+	const auto col_h = (size_h >= padding_h) ? (size_h - padding_h) / stride_h + 1 : 1;
+	const auto size_w = width + 2 * pad_w;
+	const auto padding_w = dilation_w * (kernel_w - 1) + 1;
+	const auto col_w = (size_w >= padding_w) ? (size_w - padding_w) / stride_w + 1 : 1;
 
-  // Retrieves the kernel from the compiled binary
-  auto kernel = Kernel(program_, kernel_name);
+	// Retrieves the kernel from the compiled binary
+	auto kernelOld = Kernel(program_, kernel_name);
+	tart::kernel_ptr kernel = kernelOld.get();
 
-  // Sets the kernel arguments
-  kernel.SetArgument(0, static_cast<int>(height));
-  kernel.SetArgument(1, static_cast<int>(width));
-  kernel.SetArgument(2, static_cast<int>(channels));
-  kernel.SetArgument(3, static_cast<int>(col_h));
-  kernel.SetArgument(4, static_cast<int>(col_w));
-  kernel.SetArgument(5, static_cast<int>(kernel_h));
-  kernel.SetArgument(6, static_cast<int>(kernel_w));
-  kernel.SetArgument(7, static_cast<int>(pad_h));
-  kernel.SetArgument(8, static_cast<int>(pad_w));
-  kernel.SetArgument(9, static_cast<int>(stride_h));
-  kernel.SetArgument(10, static_cast<int>(stride_w));
-  kernel.SetArgument(11, static_cast<int>(dilation_h));
-  kernel.SetArgument(12, static_cast<int>(dilation_w));
-  kernel.SetArgument(13, im_buffer());
-  kernel.SetArgument(14, static_cast<int>(im_offset));
-  kernel.SetArgument(15, col_buffer());
-  kernel.SetArgument(16, static_cast<int>(col_offset));
+	// Sets the kernel arguments
+	kernel->setArg(0, static_cast<int>(height));
+	kernel->setArg(1, static_cast<int>(width));
+	kernel->setArg(2, static_cast<int>(channels));
+	kernel->setArg(3, static_cast<int>(col_h));
+	kernel->setArg(4, static_cast<int>(col_w));
+	kernel->setArg(5, static_cast<int>(kernel_h));
+	kernel->setArg(6, static_cast<int>(kernel_w));
+	kernel->setArg(7, static_cast<int>(pad_h));
+	kernel->setArg(8, static_cast<int>(pad_w));
+	kernel->setArg(9, static_cast<int>(stride_h));
+	kernel->setArg(10, static_cast<int>(stride_w));
+	kernel->setArg(11, static_cast<int>(dilation_h));
+	kernel->setArg(12, static_cast<int>(dilation_w));
+	kernel->setArg(13, im_buffer());
+	kernel->setArg(14, static_cast<int>(im_offset));
+	kernel->setArg(15, col_buffer());
+	kernel->setArg(16, static_cast<int>(col_offset));
 
-  // Launches the kernel
-  const auto w_ceiled = Ceil(col_w, db_["COPY_DIMX"]);
-  const auto h_ceiled = Ceil(col_h, db_["COPY_DIMY"]);
-  const auto global = std::vector<size_t>{w_ceiled, h_ceiled * channels};
-  const auto local = std::vector<size_t>{db_["COPY_DIMX"], db_["COPY_DIMY"]};
-  RunKernel(kernel, queue_, device_, global, local);
+	// Launches the kernel
+	const auto w_ceiled = Ceil(col_w, db_["COPY_DIMX"]);
+	const auto h_ceiled = Ceil(col_h, db_["COPY_DIMY"]);
+	const auto global = std::vector<uint32_t>{w_ceiled/db_["COPY_DIMX"], (h_ceiled * channels)/db_["COPY_DIMY"] };
+	kernel->enqueue(global, {});
 }
 
 // =================================================================================================
@@ -108,4 +108,4 @@ template class Xim2col<float2>;
 template class Xim2col<double2>;
 
 // =================================================================================================
-}  // namespace clblast
+}	// namespace clblast
